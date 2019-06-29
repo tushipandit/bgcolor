b86dca2d049338f4567c83a74f169f12dcdcde5f
Tushar Sharma <36995938+tushipandit@users.noreply.github.com>
2019-06-29 13:53:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var _ = require('lodash');
+
+console.log("example to initate using modules");
+var a=[1,2,3,4,5,6];
+console.log(_.without(a,3));
+
+
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
